50bec812897b5ec6e3c46485e9040f7297c005e7
skamath17 <30868452+skamath17@users.noreply.github.com>
2024-08-09 16:58:00 +0000

    Added Flask project

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello, Worlds!</p>"
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
